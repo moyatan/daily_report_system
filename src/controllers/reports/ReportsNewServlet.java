54a6cc12dce9c90d4048e59f8feb9b97db9bded3
sirai <kamikami_name@yahoo.co.jp>
2020-10-23 13:29:44 +0900

    Add Report new

---
+++ src/controllers/reports/ReportsNewServlet.java
@@ -0,0 +1,48 @@
+package controllers.reports;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Report;
+
+/**
+ * Servlet implementation class ReportsNewServlet
+ */
+@WebServlet("/reports/new")
+public class ReportsNewServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReportsNewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+
+    //index.jspの新規日報登録がクリックされた時に起動
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //セッションIDをリクエストスコープに保存
+        request.setAttribute("_token", request.getSession().getId());
+
+        //新規で日報を登録する際に日付がわかった方がいいので表示
+        Report r = new Report();
+        r.setReport_date(new Date(System.currentTimeMillis()));
+        request.setAttribute("report", r);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/new.jsp");
+        rd.forward(request, response);
+    }
+
+}
